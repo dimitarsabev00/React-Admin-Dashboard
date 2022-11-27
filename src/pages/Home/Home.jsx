8b5f0710fbc24fdbed8e2b95d0e93d23e98ba8f3
Dimitar <wopz1337@gmail.com>
2022-11-27 17:18:27 +0200

    Added Home Page  Component  + Styling

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import "./Home.css";
+import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import Chart from "../../components/Chart/Chart";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/WidgetSm/WidgetSm";
+import WidgetLg from "../../components/WidgetLg/WidgetLg";
+const Home = () => {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
