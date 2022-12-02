0b349dc03a09b25bc625dd026310e504c4afd7b4
Dimitar <wopz1337@gmail.com>
2022-12-02 15:49:02 +0200

    Create Data In Firestore Database

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -1,15 +1,39 @@
 import "./New.scss";
-import Sidebar from "../../components/sidebar/Sidebar";
+import SideBar from "../../components/SideBar/SideBar";
 import NavBar from "../../components/NavBar/NavBar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 const New = ({ inputs, title }) => {
-  const [file, setFile] = useState("");
+  const [data, setData] = useState({});
+  const navigate = useNavigate();
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
+  };
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "users"), {
+        ...data,
+        timeStamp: serverTimestamp(),
+      });
+      navigate(-1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="new">
-      <Sidebar />
+      <SideBar />
       <div className="newContainer">
         <NavBar />
         <div className="top">
@@ -18,35 +42,31 @@ const New = ({ inputs, title }) => {
         <div className="bottom">
           <div className="left">
             <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
               alt=""
             />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
-                />
+                <input type="file" id="file" style={{ display: "none" }} />
               </div>
 
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                    id={input.id}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    onChange={handleInput}
+                  />
                 </div>
               ))}
-              <button>Send</button>
+              <button type="submit">Send</button>
             </form>
           </div>
         </div>
