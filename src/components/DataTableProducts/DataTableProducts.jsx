b1e09fc7bc49966714dfef5257281f8396dcbec4
Dimitar <wopz1337@gmail.com>
2022-12-03 16:08:25 +0200

    Added DataTableProducts Component + Styling

---
+++ src/components/DataTableProducts/DataTableProducts.jsx
@@ -0,0 +1,81 @@
+import "./DataTableProducts.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { productColumns } from "../../dataTableSource/dataTableSource";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../config/firebase";
+const DataTableProducts = () => {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    // LISTEN  (REALTIME DATA FROM FIREBASE FIRESTORE)
+    const unsub = onSnapshot(
+      collection(db, "products"),
+      (snapShot) => {
+        let list = [];
+        snapShot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setData(list);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return () => {
+      unsub();
+    };
+  }, []);
+  const handleDelete = async (id) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+      setData(data.filter((item) => item.id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to={`/product/test`} style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        All Products
+        <Link to="/products/new" className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={data}
+        columns={productColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default DataTableProducts;
