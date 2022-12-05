d69747e9c55c2b0cb68619bdbdc93d6f446e6a71
Dimitar <wopz1337@gmail.com>
2022-12-05 22:48:37 +0200

    Added CircularProgressbar With ChangingProgressProvider In Featured Component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -4,9 +4,9 @@ import {
   KeyboardArrowDown,
   KeyboardArrowUpOutlined,
 } from "@mui/icons-material/";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-
+import ChangingProgressProvider from "./ChangingProgressProvider";
 const Featured = () => {
   return (
     <div className="featured">
@@ -16,33 +16,46 @@ const Featured = () => {
       </div>
       <div className="bottom">
         <div className="featuredChart">
-          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+          <ChangingProgressProvider
+            values={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+          >
+            {(percentage) => (
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={buildStyles({
+                  pathTransitionDuration: 0.95,
+                  trailColor: "#82ca9d",
+                  pathColor: "#7451F8",
+                  textColor: "#7451F8",
+                })}
+              />
+            )}
+          </ChangingProgressProvider>
         </div>
         <p className="title">Total sales made today</p>
         <p className="amount">$420</p>
-        <p className="desc">
-          Previous transactions processing. Last payments may not be included.
-        </p>
+        <p className="desc">Previous transactions</p>
         <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
               <KeyboardArrowDown fontSize="small" />
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">$19.4k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlined fontSize="small" />
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">$60.4k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlined fontSize="small" />
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">$173.4k</div>
             </div>
           </div>
         </div>
