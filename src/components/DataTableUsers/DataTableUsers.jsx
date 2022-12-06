5db29e1dc2c7a396252adeeba656ccfd8288dfc2
Dimitar <wopz1337@gmail.com>
2022-12-06 18:48:10 +0200

    Added User ID In Link For View Button

--- src/components/DataTableUsers/DataTableUsers.jsx
+++ src/components/DataTableUsers/DataTableUsers.jsx
@@ -44,7 +44,7 @@ const DataTableUsers = () => {
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to={`/users/test`} style={{ textDecoration: "none" }}>
+            <Link to={`/users/${params.id}`} style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
             <div
